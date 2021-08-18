e5eb109294bfb93fef5ce4eda973a080deca7fc4
logan1x <sharmakhushal78@gmail.com>
2021-08-18 21:30:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var translate = document.querySelector("#btn-translate");
+var input_translate = document.querySelector("#input-translate");
+var output_translate = document.querySelector("#output-translate");
+
+// mock server
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// actual server
+var url = "https://api.funtranslations.com/translate/groot.json";
+
+function urlfunc(url) {
+	return url + "?" + "text=" + input_translate.value;
+}
+
+function errorhandler(error) {
+	alert("Something wrong with the server. Please try again later.");
+}
+
+function callback() {
+	fetch(urlfunc(url))
+		.then((response) => response.json())
+		.then((json) => {
+			var output_text = json.contents.translated;
+			output_translate.innerText = output_text;
+		})
+		.catch(errorhandler);
+}
+
+translate.addEventListener("click", callback);
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+	document.getElementById("toggleknop").innerHTML =
+		'<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
+	document.body.classList.add("dark-theme");
+}
+
+function changeTheme() {
+	document.body.classList.toggle("dark-theme");
+
+	document.getElementById("toggleknop").innerHTML =
+		'<i class="fas fa-moon" id="maan" style="color:#737eac;"></i>';
+
+	let theme = "light";
+	if (document.body.classList.contains("dark-theme")) {
+		document.getElementById("toggleknop").innerHTML =
+			'<i class="fas fa-sun" id="zon" style="color:#d8c658;"></i>';
+		theme = "dark";
+	}
+	localStorage.setItem("theme", theme);
+}
